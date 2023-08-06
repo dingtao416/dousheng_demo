e5a0e87208ff8c9c2b4c95ff002ed426592c1258
abuziming <255670851@qq.com>
2023-08-06 18:24:18 +0800

    初始化项目

--- controller/common.go
+++ controller/common.go
@@ -4,31 +4,63 @@ type Response struct {
 	StatusCode int32  `json:"status_code"`
 	StatusMsg  string `json:"status_msg,omitempty"`
 }
-
-type Video struct {
-	Id            int64  `json:"id,omitempty"`
-	Author        User   `json:"author"`
-	PlayUrl       string `json:"play_url" json:"play_url,omitempty"`
-	CoverUrl      string `json:"cover_url,omitempty"`
-	FavoriteCount int64  `json:"favorite_count,omitempty"`
-	CommentCount  int64  `json:"comment_count,omitempty"`
-	IsFavorite    bool   `json:"is_favorite,omitempty"`
-}
-
-type Comment struct {
-	Id         int64  `json:"id,omitempty"`
-	User       User   `json:"user"`
-	Content    string `json:"content,omitempty"`
-	CreateDate string `json:"create_date,omitempty"`
-}
-
 type User struct {
-	Id            int64  `json:"id,omitempty"`
-	Name          string `json:"name,omitempty"`
-	FollowCount   int64  `json:"follow_count,omitempty"`
-	FollowerCount int64  `json:"follower_count,omitempty"`
-	IsFollow      bool   `json:"is_follow,omitempty"`
+	Avatar          string `json:"avatar"`           // 用户头像
+	BackgroundImage string `json:"background_image"` // 用户个人页顶部大图
+	FavoriteCount   int64  `json:"favorite_count"`   // 喜欢数
+	FollowCount     int64  `json:"follow_count"`     // 关注总数
+	FollowerCount   int64  `json:"follower_count"`   // 粉丝总数
+	Id              int64  `json:"id"`               // 用户id
+	IsFollow        bool   `json:"is_follow"`        // true-已关注，false-未关注
+	Name            string `json:"name"`             // 用户名称
+	Signature       string `json:"signature"`        // 个人简介
+	TotalFavorited  string `json:"total_favorited"`  // 获赞数量
+	WorkCount       int64  `json:"work_count"`       // 作品数
 }
+
+// Video
+type Video struct {
+	Author        User   `json:"author"`         // 视频作者信息
+	CommentCount  int64  `json:"comment_count"`  // 视频的评论总数
+	CoverUrl      string `json:"cover_url"`      // 视频封面地址
+	FavoriteCount int64  `json:"favorite_count"` // 视频的点赞总数
+	Id            int64  `json:"id"`             // 视频唯一标识
+	IsFavorite    bool   `json:"is_favorite"`    // true-已点赞，false-未点赞
+	PlayUrl       string `json:"play_url"`       // 视频播放地址
+	Title         string `json:"title"`          // 视频标题
+}
+
+// Comment
+type Comment struct {
+	Content    string `json:"content"`     // 评论内容
+	CreateDate string `json:"create_date"` // 评论发布日期，格式 mm-dd
+	Id         int64  `json:"id"`          // 评论id
+	User       User   `json:"user"`        // 评论用户信息
+}
+
+//type Video struct {
+//	Id            int64  `json:"id,omitempty"`
+//	Author        User   `json:"author"`
+//	PlayUrl       string `json:"play_url" json:"play_url,omitempty"`
+//	CoverUrl      string `json:"cover_url,omitempty"`
+//	FavoriteCount int64  `json:"favorite_count,omitempty"`
+//	CommentCount  int64  `json:"comment_count,omitempty"`
+//	IsFavorite    bool   `json:"is_favorite,omitempty"`
+//}
+//type Comment struct {
+//	Id         int64  `json:"id,omitempty"`
+//	User       User   `json:"user"`
+//	Content    string `json:"content,omitempty"`
+//	CreateDate string `json:"create_date,omitempty"`
+//}
+
+//type User struct {
+//	Id            int64  `json:"id,omitempty"`
+//	Name          string `json:"name,omitempty"`
+//	FollowCount   int64  `json:"follow_count,omitempty"`
+//	FollowerCount int64  `json:"follower_count,omitempty"`
+//	IsFollow      bool   `json:"is_follow,omitempty"`
+//}
 
 type Message struct {
 	Id         int64  `json:"id,omitempty"`
