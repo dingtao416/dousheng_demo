8588fae9057964e5d698cb4a47f35f83c0de848b
abuziming <255670851@qq.com>
2023-08-06 18:35:37 +0800

    初始化项目

---
+++ dao/initDao.go
@@ -0,0 +1,33 @@
+package dao
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
+)
+
+var Db *gorm.DB
+
+// 连接数据库
+func Init() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second,  // 慢 SQL 阈值
+			LogLevel:      logger.Error, // Log level
+			Colorful:      true,         // 彩色打印
+		},
+	)
+	var err error
+	dsn := "douyin:zjqxy@tcp(43.138.25.60:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local"
+	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		log.Panicln("err:", err.Error())
+	}
+
+}
